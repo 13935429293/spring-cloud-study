56bab8122e2b12b0082c1673fc120ec2563dcbfa
WenBiao Yang <404560181@qq.com>
2017-02-11 21:15:42 +0800

    first commit | init simple server provider & constomer

---
+++ consumer-movie/src/main/java/org/ywb/study/controller/MovieController.java
@@ -0,0 +1,23 @@
+package org.ywb.study.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.ywb.study.controller.entity.User;
+
+/**
+ * Created by Administrator on 2017/2/11.
+ */
+@RestController
+public class MovieController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/movie/{id}")
+    public User findById(@PathVariable("id") Long id){
+        return this.restTemplate.getForObject("http://localhost:7900/simple/" + id, User.class);
+    }
+}
