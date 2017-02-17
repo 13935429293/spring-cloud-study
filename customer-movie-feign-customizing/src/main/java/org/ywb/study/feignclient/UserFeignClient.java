c60215cb1489285eb73514cf415e4e365389d043
yangwenbiao <yangwenbiao@passiontec.cn>
2017-02-17 14:24:55 +0800

    add eureka high availability demo

--- customer-movie-feign-customizing/src/main/java/org/ywb/study/feignclient/UserFeignClient.java
+++ customer-movie-feign-customizing/src/main/java/org/ywb/study/feignclient/UserFeignClient.java
@@ -1,5 +1,6 @@
 package org.ywb.study.feignclient;
 
+import feign.Param;
 import feign.RequestLine;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,12 +11,12 @@ import org.ywb.study.entity.User;
  * User: yangwenbiao@passiontec.cn
  * Date: 2017/2/14
  * Time: 10:35
- *
+ * see: https://github.com/OpenFeign/feign
  * 自定义 feignClient 的配置信息，和 ribbon 一样，不能将配置文件暴露在 @SpringBootApplication 扫描的包中。
  */
 @FeignClient(name = "provider-user", configuration = MyFeignConfiguration.class)
 public interface UserFeignClient {
 
     @RequestLine("GET /simple/{id}")
-    User findById(@PathVariable("id") Long id);
+    User findById(@Param("id") Long id);
 }
